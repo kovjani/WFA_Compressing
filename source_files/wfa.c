390053873a0218584d53a1b1fd009a96ff9bff65
Kovács János András <kovacs.janos212@gmail.com>
2024-09-25 22:49:14 +0200

    Open decoded image.

--- source_files/wfa.c
+++ source_files/wfa.c
@@ -5,6 +5,32 @@ int n;  // The size of matrix.
 int res; // Decoding resolution
 int size; // The size of the image (2^res)
 
+void OpenImage(char *full_path) {
+    GtkWidget *window;
+    GtkWidget *image;
+    GtkWidget *vbox;
+
+    //gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Image Loader");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
+    vbox = gtk_vbox_new(FALSE, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    image = gtk_image_new();
+    gtk_box_pack_start(GTK_BOX(vbox), image, TRUE, TRUE, 0);
+
+    gtk_image_set_from_file(GTK_IMAGE(image), full_path);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+}
+
 void SaveDecodedImage(char *directory, double *pixels_colors) {
     GdkPixbuf *pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, size, size);
     guchar *pixels = gdk_pixbuf_get_pixels(pixbuf);;
@@ -22,6 +48,8 @@ void SaveDecodedImage(char *directory, double *pixels_colors) {
     gdk_pixbuf_save(pixbuf, full_path, "png", NULL, NULL);
 
     g_print("WFA Image saved to %s\n", full_path);
+
+    OpenImage(full_path);
 
     g_object_unref(pixbuf);
     g_free(full_path);
